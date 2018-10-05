0508feac24319d2e655e3042ea59216ddcf321fe
Marcin Karski <karski80@gmail.com>
2018-10-05 13:17:53 +0100

    Working

--- GenericTableView/ViewController.swift
+++ GenericTableView/ViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
-class GenericTableViewController<T: UITableViewCell>: UITableViewController {
+class GenericTableViewController<T: GenericTableViewCell<C>, C>: UITableViewController {
+    
+    var items = [C]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -8,12 +10,45 @@ class GenericTableViewController<T: UITableViewCell>: UITableViewController {
         tableView.register(T.self, forCellReuseIdentifier: "Cell")
         
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! GenericTableViewCell<C>
+        cell.item = items[indexPath.row]
+        return cell
+    }
 }
 
-class ViewController: GenericTableViewController<UITableViewCell> {
+class GenericTableViewCell<C>: UITableViewCell {
+    
+    var item: C!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
+
+struct User {
+    let name: String
+}
+
+class UserCell: GenericTableViewCell<User> {
+    override var item: User! {
+        didSet {
+            textLabel?.text = item.name
+        }
+    }
+}
+
+class ViewController: GenericTableViewController<UserCell, User> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        items = [User(name: "Ania"), User(name: "Marcin")]
+
     }
-    
 }
